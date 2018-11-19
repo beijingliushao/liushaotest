142a531c3820698978a0d5b6ae83470c785f9726
liuqingdong <liuqingdong@DESKTOP-364OUBO>
2018-11-19 14:11:39 +0800

    第一次提交

---
+++ mytestgit/src/com/oracle/Test.java
@@ -0,0 +1,14 @@
+package com.oracle;
+/**
+ * 
+ * @author 刘庆东
+ * 2018年11月19日 下午2:07:37
+ * mytestgit
+ */
+public class Test {
+	
+	public static void main(String[] args) {
+		System.out.println("test");
+	}
+
+}
